ed4c73362ed76a741596f21790db19e7f5cdc070
kimseonkyu <lsn9410@naver.com>
2018-05-05 17:09:29 +0900

    이슈#6를 해결한 merge.py 소스코드 수정

--- sub_module/merge.py
+++ sub_module/merge.py
@@ -3,6 +3,7 @@ import cv2
 import argparse
 import os
 import glob # 특정 디렉토리에 있는 파일을 유용하게 사용하게 도와줌
+from PIL import Image
 import numpy as np
 
 parser = argparse.ArgumentParser(description='image rotation')
@@ -11,29 +12,6 @@ parser.add_argument('--src2', dest='src_dir2', required=True, help='path of the 
 parser.add_argument('--dst', dest='dst_dir', required=True, help='path of the target img')
 args = parser.parse_args()
 
-def merge(p1, p2):
-	image1 = cv2.imread(p1)
-	image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
-				
-	image2 = cv2.imread(p2)
-	image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-
-	height1, width1 = image1.shape[:2]
-	height2, width2 = image2.shape[:2]
-
-	result = np.concatenate((image1, image2), axis=1)
-
-	return result
-
-def path2file_name(p):
-	label = os.path.basename(p).split("_")
-	file_name=""
-	for a in label:
-		file_name+=str(a)
-	file_name = file_name.split('.jpg')[0]
-
-	return file_name
-	
 if __name__=="__main__":
 	dst_dir = args.dst_dir
 	src_dir1 = args.src_dir1
@@ -43,18 +21,24 @@ if __name__=="__main__":
 		os.makedirs(dst_dir)
 
 	paths1 = glob.glob(os.path.join(src_dir1, "*.jpg"))
-	paths2 = glob.glob(os.path.join(src_dir2, "*.jpg"))
 	
 	for p1 in paths1:
-		name1 = path2file_name(p1)
-
-		for p2 in paths2:
-			name2 = path2file_name(p2)
-			if name1 == name2:
-				if dst_dir[-1] != '/':
-					dst_dir += '/'
-				cv2.imwrite(str(dst_dir)+name1+'_merge.jpg',merge(p1,p2))
-
+		label1 = os.path.basename(p1).split('.')[0]
+		p2 = os.path.join(src_dir2, label1 + '.jpg')
 		
+		try:
+			image1 = cv2.imread(p1)
+			image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+			image2 = cv2.imread(p2)
+			image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
 
+			height1, width1 = image1.shape[:2]
+			height2, width2 = image2.shape[:2]
 
+			if dst_dir[-1] != '/':
+				dst_dir += '/'
+			vis = np.concatenate((image1, image2), axis=1)
+			cv2.imwrite(str(dst_dir)+label1+'_merge_.jpg',vis)
+			print('Success : ' + str(dst_dir) + label1 + '_merge_.jpg')
+		except:
+			print('Fail : ' + label1 + 'File is not exist in src2_dir')
