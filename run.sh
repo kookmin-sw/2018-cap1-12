72d253e5f8983f4504d0119ef40e239d64e743c0
leekitten123 <leekitten123@gmail.com>
2018-05-18 15:31:05 +0900

    add 는 은 train

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+work_dir=$( cd "$(dirname "$0")" ; pwd )
 
-cd ${HOME}/Downloads/git/2018-cap1-12/sub_module/svg2ttf/
 
+# test
+cd DiscoGAN-pytorch
+python main.py --dataset=sms_dms --load_path=logs/sms_dms/ --is_train=False
+
+
+# move
+cd ${work_dir}
+mv DiscoGAN-pytorch/logs/sms_dms/test/0_x_A.png sub_module/svg2ttf/data/B294.png
+mv DiscoGAN-pytorch/logs/sms_dms/test/0_x_AB.png sub_module/svg2ttf/data/C740.png
+
+
+# svg2ttf
+cd ${work_dir}/sub_module/svg2ttf/
 python3 main.py
-
-
-
-
-
-
-
