d39ec1297146299eef8a1c83865844a7afba405b
Hwangyonghwan <iamyonghwan@nate.com>
2018-05-27 21:33:22 +0900

    #19 Back-End : flask multiProcessing

--- flask_server/app.py
+++ flask_server/app.py
@@ -132,5 +132,5 @@ def footer():
 	return render_template('footer.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1024, debug = True, threaded=True)
+    app.run(host='0.0.0.0', port=1024, debug = True, processes=16)
     
